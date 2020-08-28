9e0e2d524e0d8fa23fe527c7a52f125a3a9eab19
vpaladino778 <vpaladino778@gmail.com>
2020-08-28 15:03:57 -0400

    Replaced jumbotron with parralax rowing image

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,4 @@
+// Scroll down button
 $(document).ready(function(){
   $("a").on('click', function(event) {
     if (this.hash !== "") {
@@ -10,4 +11,14 @@ $(document).ready(function(){
       });
     }
   })
-}) 
+})
+// Jumbotron parralax 
+var jumboHeight = $('.jumbotron').outerHeight();
+function parallax(){
+    var scrolled = $(window).scrollTop();
+    console.log("Scrolled: " + scrolled + "CSS value: " + $('.bg').css('height'));
+    $('.bg').css('height', (jumboHeight-scrolled) + 'px');
+};
+$(window).scroll(function(e){
+    parallax();
+});
